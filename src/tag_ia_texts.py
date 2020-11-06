673d60b326239740eab60ce2842bb8c78398730d
fechan <edu.fechan@gmail.com>
2020-11-05 22:08:18 -0800

    write txt and tei to new files

--- src/tag_ia_texts.py
+++ src/tag_ia_texts.py
@@ -1,13 +1,19 @@
 """tag_ia_texts.py - Downloads raw text of an Internet Archive book and TEI tags it
 """
-import internetarchive as ia
+import io
 import re
-from ner.flair_ner import tag_entities, tagger
-from tei.assemble_tei import create_header, create_xml, create_body
+from internetarchive import get_files
+from ner.flair_ner import tagger
+from tei.assemble_document import create_document
 
 ia_idents = ["reminiscencesoft00tangrich"]
 
 for ident in ia_idents:
-    files = ia.get_files(ident, glob_pattern="*djvu.txt", formats="txt")
+    files = get_files(ident, glob_pattern="*djvu.txt", formats="txt")
     txt_file = next(files)
-    txt_file.download()
+    txt_file.download(file_path=f"./txt_files/{ident}.txt")
+    with open(f"./tei_files/{ident}.tei", "w") as output_file:
+        with open(f"./txt_files/{ident}.txt", "r") as book:
+            content = book.read()
+        output_file.write(create_document(content))
+tagger.close()
