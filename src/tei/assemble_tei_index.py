a71dba0ae027c0a539c52c86f8cdbd07e306e40f
fechan <edu.fechan@gmail.com>
2021-01-06 22:13:59 -0800

    add get_person_tag

--- src/tei/assemble_tei_index.py
+++ src/tei/assemble_tei_index.py
@@ -11,6 +11,7 @@ class IndexAssembler:
         wl_session -- an active Wolfram kernel session
         """
         self.session = wl_session
+        self.dateformat = wl.List('Year', '-', 'Month', '-', 'Day')
 
     def create_index(self, seen_entities):
         """Generate a TEI index out of the seen_entities of a NamedEntityRecognizer"""
@@ -23,16 +24,103 @@ class IndexAssembler:
                 soup.find('listOrg', attrs={'type': 'Company'}).append(org)
             elif entity_type == 'People':
                 person = self.get_person_tag(soup, mathematica_urn, xml_id, interpretation)
-                soup.find('listPerson', attrs={'type': 'People'}).append(person)
+                soup.find('listPerson', attrs={'type': 'Person'}).append(person)
         return str(soup.prettify())
 
-    def get_company_tag(self, soup, mathematica_urn, xml_id, interpretation):
-        """Generate a TEI tag for a company
+    def get_person_tag(self, soup, mathematica_urn, xml_id, interpretation):
+        """"Generate the a TEI index entry for a person
 
         Looks like:
-        <org xml:id="VARIABLE companyId">
-            <orgName>VARIABLE Company name</orgName>
-            <note>VARIABLE Notable information about the company</note>
+        <person xml:id="Alice" sex="f">
+            <persName>Alice</persName>
+            <birth when="1900-01-01">
+                <placeName>Cairo, Egypt</placeName>
+            </birth>
+            <death when="1900-01-01">
+                <placeName>Cairo, Egypt</placeName>
+            </death>
+            <occupation type="actor"/> <!-- multiple of these can exist here -->
+            <note>Notable information about Alice</note>
+        </person>
+        """
+        gender = self.session.evaluate(
+            wl.EntityValue(
+                wl.EntityValue(interpretation, 'Gender'),
+                'Name'
+            )
+        )
+        person = soup.new_tag('person', attrs={
+            'xml:id': xml_id,
+            'ref': mathematica_urn,
+            'sex': gender[0]
+        })
+
+        persName = soup.new_tag('persName')
+        name = self.session.evaluate(wl.EntityValue(interpretation, "Name"))
+        persName.append(name)
+        person.append(persName)
+
+        birthday = self.session.evaluate(
+            wl.DateString(
+                wl.EntityValue(interpretation, "BirthDate"),
+                self.dateformat
+            )
+        )
+        birth = soup.new_tag('birth', attrs={'when': birthday})
+        placeName = soup.new_tag('placeName')
+        birthplace = self.session.evaluate(
+            wl.CityData(
+                wl.EntityValue(interpretation, "BirthPlace"),
+                "FullName"
+            )
+        )
+        placeName.append(birthplace)
+        birth.append(placeName)
+        person.append(birth)
+
+        deathday = self.session.evaluate(
+            wl.DateString(
+                wl.EntityValue(interpretation, "DeathDate"),
+                self.dateformat
+            )
+        )
+        death = soup.new_tag('death', attrs={'when': deathday})
+        placeName = soup.new_tag('placeName')
+        deathplace = self.session.evaluate(
+            wl.CityData(
+                wl.EntityValue(interpretation, "DeathPlace"),
+                "FullName"
+            )
+        )
+        placeName.append(deathplace)
+        death.append(placeName)
+        person.append(death)
+
+        occupations = self.session.evaluate(wl.EntityValue(interpretation, "Occupation"))
+        for occupation_name in occupations:
+            occupation = soup.new_tag("occupation", attrs={'type': occupation_name})
+            person.append(occupation)
+
+        note = soup.new_tag('note')
+        notable_facts = session.evaluate(
+            wl.Map(
+                wl.ToString,
+                wl.EntityValue(interpretation, "NotableFacts")
+            )
+        )
+        note.append(notable_facts)
+        person.append(note)
+
+        return person
+
+
+    def get_company_tag(self, soup, mathematica_urn, xml_id, interpretation):
+        """Generate a TEI index entry for a company.
+
+        Looks like:
+        <org xml:id="companyId">
+            <orgName>Company name</orgName>
+            <note>Notable information about the company</note>
         </org>
         """
         org = soup.new_tag('org', attrs={
@@ -41,14 +129,14 @@ class IndexAssembler:
         })
                 
         orgName = soup.new_tag('orgName')
-        name = self.session.evaluate(wl.System.EntityValue(interpretation, "Name"))
+        name = self.session.evaluate(wl.EntityValue(interpretation, "Name"))
         orgName.append(name)
         org.append(orgName)
 
         note = soup.new_tag('note')
-        industry = self.session.evaluate(wl.System.EntityValue(interpretation, "Industry"))
-        founded = self.session.evaluate(wl.System.DateString(wl.System.EntityValue(interpretation, "FoundingDate")))
-        status = self.session.evaluate(wl.System.EntityValue(interpretation, "Status")).lower()
+        industry = self.session.evaluate(wl.EntityValue(interpretation, "Industry"))
+        founded = self.session.evaluate(wl.DateString(wl.EntityValue(interpretation, "FoundingDate")))
+        status = self.session.evaluate(wl.EntityValue(interpretation, "Status")).lower()
         note.append(f"{industry} company founded in {founded}. Currently {status}.")
         org.append(note)
 
